8b9ed142645f1c32e86b3da4cd546e0f740a1968
Nick Hong <nickhong@originprotocol.com>
2022-09-12 13:57:55 -0400

    Remove lib

--- lib/components/Header/OriginLogo.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="react" />
-interface OriginLogoProps {
-    webProperty: 'originprotocol' | 'ousd' | 'story';
-    height?: number;
-    theme?: 'light' | 'dark';
-}
-declare const OriginLogo: ({ webProperty, height, theme }: OriginLogoProps) => JSX.Element;
-export default OriginLogo;
