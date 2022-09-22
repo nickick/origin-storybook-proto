52bc7e403324cda954112d83d7b0926df22398d3
rolandpo <rolopod@gmail.com>
2022-09-22 14:16:48 +0100

    add altcard and typography

---
+++ src/components/AltCard/index.tsx
@@ -0,0 +1,69 @@
+import { Typography } from "../Typography"
+
+export interface AltCardProps {
+  /**
+   * String at the top of the text portion of the card
+   */
+  title: string
+  /**
+   * String used as body/subtitle below title
+   */
+  body?: string
+  /**
+   * Src for image at top of card
+   */
+  imgSrc: string
+  /**
+   * Src for alt at top of card
+   */
+  imgAlt: string
+  /**
+   * Toggle smaller font
+   */
+  narrow: boolean
+  /**
+   * Src for image in thumbnail at top of card, only for narrow cards
+   */
+  thumbnailSrc?: string
+  /**
+  * Alt for image in thumbnail at top of card, only for narrow cards
+  */
+  thumbnailAlt?: string
+}
+
+export const AltCard = ({
+  body,
+  imgSrc,
+  imgAlt,
+  title,
+  narrow,
+  thumbnailSrc,
+  thumbnailAlt,
+}: AltCardProps) => (
+  <div className='bg-gray-100 p-6 rounded-xl w-full shadow overflow-hidden text-black'>
+    <div className='relative float-left mb-3'>
+      <img src={imgSrc} alt={imgAlt} className="h-10" />
+      { thumbnailSrc && (
+        <div className='absolute right-0 bottom-0 transform translate-x-1/2 translate-y-1/2 overflow-hidden rounded-full border-2 border-white'>
+          <img src={thumbnailSrc} alt={thumbnailAlt} className="w-5 h-5 object-cover" />
+        </div>
+      )}
+    </div>
+    <div className="clear-both">
+      <div className="text-blue-500">
+        <Typography.Caption2>
+          {title}
+        </Typography.Caption2>
+      </div>
+      {narrow ? (
+        <Typography.Title2 classes={''}>
+          {body}
+        </Typography.Title2>
+        ) : (
+        <Typography.Title classes={''}>
+          {body}
+        </Typography.Title>
+      )}
+    </div>
+  </div>
+)
